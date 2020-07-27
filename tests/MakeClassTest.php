3927cabd71906b5898d55fc55e2ac73cc5d7d8b9
Stephen Jude <StephenJudesuccess@gmail.com>
2020-07-27 01:32:55 +0100

    Add output assertion method

--- tests/MakeClassTest.php
+++ tests/MakeClassTest.php
@@ -11,12 +11,14 @@ class MakeClassTest extends TestCase
     public function test_make_class()
     {
         $this->artisan('make:class Helper')
+            ->expectsOutput('Trait created successfully.')
             ->assertExitCode(0);
     }
 
     public function test_make_class_with_namespace()
     {
         $this->artisan('make:class Supports/EmailSupport')
+        ->expectsOutput('Class created successfully.')
             ->assertExitCode(0);
     }
 }
--- tests/MakeTraitTest.php
+++ tests/MakeTraitTest.php
@@ -7,6 +7,8 @@ class MakeTraitTest extends TestCase
     /** @test */
     public function test_make_trait()
     {
-        $this->artisan('make:trait FileUplaod')->assertExitCode(0);
+        $this->artisan('make:trait FileUplaod')
+            ->expectsOutput('Trait created successfully.')
+            ->assertExitCode(0);
     }
 }
